6227c368d67db242dca73cfc0a941a9af619bf8b
Viktor40 <vannieuwenhuize.viktor@gmail.com>
2021-12-15 11:01:12 +0100

    Changed example mod settings to work with this specific mod

--- src/main/java/fixmc188328/FixMc188328.java
+++ src/main/java/fixmc188328/FixMc188328.java
@@ -1,21 +1,17 @@
-package net.fabricmc.example;
+package fixmc188328;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class FixMc188328 implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("FixMC188328");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Fix to MC-188328 is applied");
 	}
 }
