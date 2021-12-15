d8afb6a96ac1f73401a256526974323d117d9c4b
Viktor40 <vannieuwenhuize.viktor@gmail.com>
2021-12-15 11:38:22 +0100

    Implemented a better method to fix the bug

--- src/main/java/fixmc188328/mixin/BoatEntityMixin.java
+++ src/main/java/fixmc188328/mixin/BoatEntityMixin.java
@@ -3,10 +3,12 @@ package fixmc188328.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BoatEntity.class)
@@ -15,14 +17,16 @@ public abstract class BoatEntityMixin extends Entity {
         super(type, world);
     }
 
-    @Inject(method = "pushAwayFrom", at = @At("TAIL"))
-    public void fixBoatBug(Entity entity, CallbackInfo ci) {
-        if (!(entity instanceof BoatEntity)) {
-            if (!(entity.getBoundingBox().minY <= this.getBoundingBox().minY)) {
-                if (entity.getBoundingBox().minY <= this.getBoundingBox().maxY) {
-                    super.pushAwayFrom(entity);
-                }
-            }
-        }
+    @Redirect(
+            method = "pushAwayFrom",
+            at = @At(
+                    value = "FIELD",
+                    target = "net/minecraft/util/math/Box.minY:D",
+                    ordinal = 2
+            )
+    )
+    public double redirectBoxMinY(Box instance) {
+        return this.getBoundingBox().maxY;
     }
+
 }
