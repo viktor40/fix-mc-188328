17217bed6c4f6b7e0f10de6a43d071fe1cd6ff89
Viktor40 <vannieuwenhuize.viktor@gmail.com>
2021-12-15 11:00:41 +0100

    Added fix for the bug

---
+++ src/main/java/fixmc188328/mixin/BoatEntityMixin.java
@@ -0,0 +1,30 @@
+package fixmc188328.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BoatEntity.class)
+public abstract class BoatEntityMixin extends Entity {
+    public BoatEntity thisBoatEntity = (BoatEntity)(Object)this;
+
+    public BoatEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "pushAwayFrom", at = @At("TAIL"))
+    public void fixBoatBug(Entity entity, CallbackInfo ci) {
+        if (!(entity instanceof BoatEntity)) {
+            if (!(entity.getBoundingBox().minY <= this.getBoundingBox().minY)) {
+                if (entity.getBoundingBox().minY <= this.thisBoatEntity.getBoundingBox().maxY) {
+                    super.pushAwayFrom(entity);
+                }
+            }
+        }
+    }
+}
